d1b01b00d38fbf9a232020c36467113b0a2f749c
onebaldman <onebaldman@gmail.com>
2015-03-04 09:05:22 -0600

    Jeremy commit of extracted "Boo" project.
    Added project called "Jeremy Assets" that contains models to potentially start from.

---
+++ Receptor Test/Assets/Scripts/receptorScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class receptorScript : MonoBehaviour {
+
+	public GameObject _ActiveReceptor;
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "ECP") 
+		{
+			//Destroy (other.gameObject);
+			this.gameObject.SetActive(false);
+			// if the player entered the trigger... 
+			// create the object and get a reference to it: 
+			GameObject NewAReceptor = (GameObject)Instantiate(_ActiveReceptor, transform.position, transform.rotation); 
+			// play the sound in the trigger AudioSource:
+			Destroy (other.gameObject);
+
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+	
+	}
+
+
+}
